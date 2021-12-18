7541075c35e6efcc24758d48fe067cd78a76f5af
cudderson <codered1227@gmail.com>
2021-12-18 00:05:31 -0600

    reorganize code, refactor Pokemon factory => constructor, improve performance

--- pokemon-randomizer/pokemon.js
+++ pokemon-randomizer/pokemon.js
@@ -2,19 +2,31 @@
 
 // current: should modularize this for readability
 
+// factory function version
+// const Pokemon = (data) => {
+//   const name = capitalizeName(data.name);
+//   const avatar = data.sprites.front_default;
+//   return { name, avatar };
+// };
 
-// factory function
-const Pokemon = (data) => {
-  const name = capitalizeName(data.name);
-  const avatar = data.sprites.front_default;
-  return { name, avatar };
-};
-
-function capitalizeName(name) {
-  const first_letter = name.charAt(0).toUpperCase();
-  const new_name = first_letter + name.slice(1);
-  return new_name;
+// constructor version
+function Pokemon(data) {
+  this.name = data.name;
+  this.avatar = data.sprites.front_default;
 }
+
+Pokemon.prototype.capitalizeName = function() {
+  const first_letter = this.name.charAt(0).toUpperCase();
+  const new_name = first_letter + this.name.slice(1);
+  this.name = new_name;
+}
+
+// same function as above, no attachment to constructor
+// function capitalizeName(name) {
+//   const first_letter = name.charAt(0).toUpperCase();
+//   const new_name = first_letter + name.slice(1);
+//   return new_name;
+// }
 
 async function getRandomPokemon() {
   try {
@@ -24,36 +36,58 @@ async function getRandomPokemon() {
       { mode: "cors" }
     );
     const data = await response.json();
+
     // construct Pokemon object
-    const pokemon = Pokemon(data);
+    const pokemon = new Pokemon(data);
+    pokemon.capitalizeName();
+
     return pokemon;
+
   } catch (error) {
     console.log(error);
   }
 }
 
-// get transition name to ensure it will work on all browsers
-function transitionEndEventName() {
-  var i,
-    undefined,
-    el = document.createElement("div"),
-    transitions = {
-      transition: "transitionend",
-      OTransition: "otransitionend", // oTransitionEnd in very old Opera
-      MozTransition: "transitionend",
-      WebkitTransition: "webkitTransitionEnd",
-    };
+async function displayPokemon(allPokemon) {
+  for (let i = 0; i < 6; i++) {
+    const pokemon_object = await getRandomPokemon();
 
-  for (i in transitions) {
-    if (transitions.hasOwnProperty(i) && el.style[i] !== undefined) {
-      return transitions[i];
+    allPokemon[i].getElementsByTagName("span")[0].textContent = pokemon_object.name;
+
+    const pokemon_avatar = allPokemon[i].getElementsByTagName("img")[0];
+    pokemon_avatar.src = pokemon_object.avatar;
+
+    // only await on last image
+    if (i === 5) {
+      await new Promise((resolve) => {
+        pokemon_avatar.onload = resolve;
+      });
     }
   }
-
-  //TODO: throw 'TransitionEnd event is not supported in this browser';
 }
 
-function init() {
+function addListeners() {
+  const containerInner = document.getElementById("container-inner");
+  const getPokemonBtn = document.getElementById("fetch-pokemon");
+  const pokemonContainers = document.getElementsByClassName("container-poke");
+
+  // generate new pokemon when container is hidden, then show when complete
+  containerInner.addEventListener('transitionend', () => {
+    if (containerInner.classList.contains("hide")) {
+      displayPokemon(pokemonContainers).then(() => {
+        containerInner.classList.toggle("hide");
+      });
+    }
+  });
+
+  // clicking button triggers transition event
+  getPokemonBtn.addEventListener("click", async () => {
+    containerInner.classList.toggle("hide");
+  });
+}
+
+// IIFE
+(function initializeDOMElements() {
   const container_inner = document.getElementById("container-inner");
 
   for (let i = 0; i < 6; i++) {
@@ -72,41 +106,9 @@ function init() {
 
     container_inner.appendChild(pokemon_div);
   }
-}
 
-init();
-const getPokemonBtn = document.getElementById("fetch-pokemon");
-const containerInner = document.getElementById("container-inner");
-const allPokemon = document.getElementsByClassName("container-poke");
+  // add listeners to DOM elements
+  addListeners();
+})();
 
-const transitionEnd = transitionEndEventName();
-containerInner.addEventListener(transitionEnd, () => {
-  if (containerInner.classList.contains('hide')) {
-    displayPokemon().then(() => toggleVisibility());
-  }
-});
-
-async function displayPokemon() {
-  for (let i = 0; i < 6; i++) {
-    const pokemon_object = await getRandomPokemon();
-
-    allPokemon[i].getElementsByTagName("span")[0].textContent = pokemon_object.name;
-
-    const pokemon_avatar = allPokemon[i].getElementsByTagName("img")[0]; 
-    pokemon_avatar.src = pokemon_object.avatar;
-
-    // only await on last image
-    if (i === 5) {
-      await new Promise((resolve) => {
-        pokemon_avatar.onload = resolve;
-      });
-    }
-  }
-  console.log("pokemon retrieved!!!")
-}
-
-async function toggleVisibility() {
-  containerInner.classList.toggle("hide");
-}
-
-getPokemonBtn.addEventListener("click", toggleVisibility);
+// initializeDOMElements();

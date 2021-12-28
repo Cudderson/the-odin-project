d11fc0e0ea57d90a0950d283da2fc26ba854f65b
cudderson <codered1227@gmail.com>
2021-12-28 17:21:21 -0600

    retrieve cards based on current level, only display when all are loaded to DOM

--- pokemon/pokemon-memory/src/App.js
+++ pokemon/pokemon-memory/src/App.js
@@ -1,11 +1,39 @@
 import PokemonCard from './PokemonCard.js'; 
-import './App.module.css';
+import styles from './App.module.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [level, setLevel] = useState(1);
+  const [pokemonList, setPokemonList] = useState(null);
+  
+  useEffect(() => {
+    // on initial render, should fetch pokemon for level 1
+    const pokemonRequired = (level * 2) + 2;
+    const tempList = [];
+
+    for (let i = 0; i < pokemonRequired; i++) {
+      const card = <PokemonCard />;
+      tempList.push(card);
+    }
+
+    setPokemonList(tempList);
+  }, [level]);
+
+  const checkComplete = (index) => {
+    const container = document.getElementById(styles['main-container']);
+
+    if (index === container.children.length - 1) {
+      container.classList.remove(styles['hide']);
+      container.classList.add(styles['show']);
+    }
+  }
+
   return (
-    <>
-      <PokemonCard />
-    </>
+    <ul id={styles['main-container']} className={styles['hide']}>
+      {pokemonList ? pokemonList.map((pokeCard, index) => {
+        return <li onLoad={() => checkComplete(index)} key={index}>{pokeCard}</li>
+      }) : null}
+    </ul>
   );
 }
 

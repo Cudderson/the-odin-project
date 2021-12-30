9a10b0c944349067b9ee6b5c057477e0ef8f8717
cudderson <codered1227@gmail.com>
2021-12-30 14:11:52 -0600

    level system working, improve styling transitions on new level

--- pokemon/pokemon-memory/src/App.js
+++ pokemon/pokemon-memory/src/App.js
@@ -10,9 +10,14 @@ function App() {
   const [pokemonFound, updatePokemonFound] = useState(new Set());
 
   useEffect(() => {
+    // make sure container is hidden when new level begins
+    determineVisibility('new_level');
+
     console.log("useEffect called");
     const pokemonRequired = (level * 2) + 2;
     const tempList = [];
+
+    updatePokemonFound(new Set());
 
     // async IIFE
     (async () => {
@@ -25,12 +30,16 @@ function App() {
     })();
   }, [level]);
 
-  const checkComplete = (index) => {
+  const determineVisibility = (index) => {
     const container = document.getElementById(styles["main-container"]);
 
     if (index === container.children.length - 1) {
       container.classList.remove(styles["hide"]);
       container.classList.add(styles["show"]);
+    }
+    else if (index === 'new_level') {
+      container.classList.remove(styles['show']);
+      container.classList.add(styles['hide']);
     }
   };
 
@@ -41,15 +50,19 @@ function App() {
       updatePokemonFound(pokemonFound.add(pokemonClicked));
       console.log(pokemonFound);
       if (pokemonFound.size === (level * 2) + 2) {
+        
         // player wins the level
         console.log("you won the level!");
-        return; // this will trigger the next step (new level)
+
+        // update and trigger new level ***
+        setLevel(level + 1);
+        return;
       }
     } 
     else {
       console.log(`pokemonFound already has ${pokemonClicked}! Game Over!`);
       document.getElementById(pokemonClicked).style.background = 'red';
-      return; // this should also trigger something
+      return; // this should also trigger something (TODO)
     }
 
     // we should also shuffle the order in which the elements appear on screen
@@ -63,7 +76,7 @@ function App() {
             return (
               <li
                 onClick={handleClick}
-                onLoad={() => checkComplete(index)}
+                onLoad={() => determineVisibility(index)}
                 key={index}
               >
                 <PokemonCard pokemonData={pokeData} />

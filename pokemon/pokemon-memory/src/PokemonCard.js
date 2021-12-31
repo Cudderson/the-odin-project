9d99cc7fd6151424905a2e208ab23a81b71fd2da
cudderson <codered1227@gmail.com>
2021-12-30 19:22:32 -0600

    improve responsiveness, add additional styling

--- pokemon/pokemon-memory/src/PokemonCard.js
+++ pokemon/pokemon-memory/src/PokemonCard.js
@@ -2,16 +2,21 @@ import styles from "./PokemonCard.module.css";
 
 const PokemonCard = ({pokemonData}) => {
 
-  function displayCard (pokemonToDisplayId) {
+  function displayCard(pokemonToDisplayId) {
     // display card after image has been loaded
     document.getElementById(pokemonToDisplayId).classList.remove(styles['hide']);
+  }
+
+  function capitalizeName(name) {
+    const first_char = name.charAt(0);
+    return `${first_char.toUpperCase().concat(name.slice(1))}`;
   }
 
   return (
     pokemonData ? (
       <div id={pokemonData.name} className={[styles['card-container'], styles['hide']].join(' ')}>
         <img onLoad={() => displayCard(pokemonData.name)} src={pokemonData.sprites.front_default} alt=""></img>
-        <span>{pokemonData.name}</span>
+        <span>{capitalizeName(pokemonData.name)}</span>
       </div>
     ) : null
   );
